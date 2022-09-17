af6e9fd4642529bb0a65d5749c4d2506b666eb92
Eliecer Aguilar <eliecer.aguilar1@utp.ac.pa>
2022-09-16 23:47:41 -0500

    creacion del modelo de la db

--- prueba_tecnica/settings.py
+++ prueba_tecnica/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'todoApp',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -76,8 +77,12 @@ WSGI_APPLICATION = "prueba_tecnica.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'eliecer_django',
+        'USER': 'django_prueba',
+        'PASSWORD': 'eliecer-django',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
 }
 
