e07a84d8a4572594ba0bfeb5ee5a4321d5fddde3
Eliecer Aguilar <eliecer.aguilar1@utp.ac.pa>
2022-09-18 18:24:47 -0500

    ulrs main page

--- prueba_tecnica/urls.py
+++ prueba_tecnica/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('tasks/', include('todoApp.urls'))
+    path('', include('todoApp.urls')),
+
 ]
--- todoApp/urls.py
+++ todoApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import list_tasks,create_tasks
+from .views import list_tasks, create_tasks
 
 
 urlpatterns = [
-    path('', list_tasks),
+    path('', list_tasks, name="main_page"),
     path('new_task/', create_tasks, name="create_new_task")
+
 ]
