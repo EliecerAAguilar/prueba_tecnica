d717e4d01fe08c75f3302e97f0d5f6655339feee
Eliecer Aguilar <eliecer.aguilar1@utp.ac.pa>
2022-09-18 19:19:26 -0500

    fixed error redirect url

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -4,6 +4,5 @@ from .views import list_tasks, create_tasks
 
 urlpatterns = [
     path('', list_tasks, name="main_page"),
-    path('new_task/', create_tasks, name="create_new_task")
-
+    path('/new_task/', create_tasks, name="create_new_task")
 ]
--- todoApp/views.py
+++ todoApp/views.py
@@ -4,9 +4,10 @@ from .models import task
 # Create your views here.
 
 def list_tasks(request):
-    return render(request, 'list_tasks.html')
+    tasks = task.objects.all()
+    return render(request, 'list_tasks.html', {"tasks": tasks})
 
 def create_tasks(request):
     tasks = task(title=request.POST['form_title'], description=request.POST['form_description'])
     tasks.save()
-    return redirect('/tasks/')
+    return redirect('/')
